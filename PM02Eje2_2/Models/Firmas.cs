e72461885aaf35f62bc7f7a86f9384523a3e5bda
Tingo <86501910+apazuth01@users.noreply.github.com>
2022-07-15 08:15:20 -0600

    Agregar archivos de proyecto.

---
+++ PM02Eje2_2/Models/Firmas.cs
@@ -0,0 +1,24 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace PM02Eje2_2.Models
+{
+    public class Firmas
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public String Nombre { get; set; }
+
+        [MaxLength(100)]
+        public String Descripcion { get; set; }
+
+        public Byte[] Imagen { get; set; }
+    }
+}
